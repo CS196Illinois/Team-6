2b179879100a6d6f964f430d42132ffaac629769
LongpingZhang <60148828+LongpingZhang@users.noreply.github.com>
2020-03-08 15:01:59 -0500

    Delete main.js

--- frontend/test-site/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// "Get Information" button
-// It nows can redirect users to "second-page.html".
-// What about fetching information about the car? Should we do that in the javascript for "second-page.html"?
-let getInfoButton = document.querySelector('button');
-getInfoButton.onclick = function () {
-    location.href = "frontend/test-site/second-page.html"
-}
-
