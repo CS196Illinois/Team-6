a0e5c3af31cddbc6188e3c60ad99ff2d5d069ef0
Longping Zhang <longpingzhang@LongpingdeMacBook-Pro.local>
2020-04-12 14:57:10 -0500

    Post data and display response

--- frontend/test-site/scripts/index-script.js
+++ frontend/test-site/scripts/index-script.js
@@ -1,25 +1,54 @@
 //Display image chosen through the input element in "#img-output".
 const plusButton = document.getElementById("img-find-button");
-plusButton.onchange = function (event) {
-    let image = document.getElementById('img-output');
-    image.src = URL.createObjectURL(event.target.files[0]);
+plusButton.onchange = function () {
+    let uploadImg = document.getElementById('img-output');
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function() {
+        uploadImg.src = reader.result;
+    }
+    reader.readAsDataURL(file);
 } 
 
-//Prevent uploading if no image is chosen and pop up an alert message.
-const uploadImg = document.getElementById("img-output");
-const form = document.querySelector("form");
-form.onsubmit = function (event) {
+// Send a post request
+const classifyButton = document.getElementById("classify-button");
+classifyButton.onclick = function() {
+    // Pop up an alert if no image is chosen.
+    const uploadImg = document.getElementById("img-output");
     if (uploadImg.src === "") {
-        event.preventDefault();
         alert("You must choose an image to classify.");
+        return;
     }
+
+    var xhr = new XMLHttpRequest();
+    // url ??????
+    xhr.open('POST', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        // if the operation is completed
+        if (this.readyState === xhr.DONE) {
+            var dataAsJson = xhr.response;
+            // Display JsonObject in a readable way
+            display(dataAsJson);
+        }
+    }
+
+    // Get the file of the selected image
+    var image = plusButton.files[0];
+    // Convert the image into URL
+    var reader = new FileReader();
+    reader.onload = function() {
+        var imageURL = reader.result;
+        fileData.append('file', imageURL)
+    }
+    reader.readAsDataURL(image);
+    // Send the selected image to server
+    var fileData = new FormData();
+    xhr.send(fileData);
 }
 
-//Get a jsonObject from server to extract and display data.
-fetch(url).then(function(response) {
-    //Return the response as an jsonObject
-    return response.json();
-}).then(function(json) {
+//Helper function to display extracted data.
+function display(jsonObject) {
     //Make "description" and "pictureDropBox" disaapear.
     const description = document.getElementById("description");
     description.style.display = "none";
@@ -32,12 +61,6 @@ fetch(url).then(function(response) {
     const parent = document.querySelector("main");
     parent.append(dataBox);
 
-    let data = json;
-    display(data);
-});
-
-//Helper function to display extracted data.
-function display(jsonObject) {
     for (info in jsonObject) {
         let newElement = document.createElement("p");
         newElement.textContent = `${info}: ${jsonObject[info]}`;
