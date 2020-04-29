713b11e4233bf43aa2bd5c24c4301570877dd9be
derivint <60678900+derivint@users.noreply.github.com>
2020-04-29 11:53:47 -0500

    Update servermain.py

--- servermain.py
+++ servermain.py
@@ -27,7 +27,7 @@ def homepage():
 		imageToPassTensor = toTensor(imageToPass)
 		pred = model(imageToPassTensor[None, ...])
 		print(pred)
-		print(pred.grad_fn.data)
+		print(pred.grad)
 		return redirect(url_for("imageprocessed"))
 	else:
 		return render_template("index.html")
